d5f7e0e7366bbe0ab784abd4b355470027380f11
ZainZhu <370262452@qq.com>
2017-08-04 18:29:38 +0800

    reformat var_args.py

--- samples/function/kw_args.py
+++ samples/function/kw_args.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
 def print_scores(**kw):
     print('      Name  Score')
     print('------------------')
     for name, score in kw.items():
-        print('%10s  %d' % (name, score))
+        print('%-10s  %d' % (name, score))
     print()
+
 
 print_scores(Adam=99, Lisa=88, Bart=77)
 
 data = {
     'Adam Lee': 99,
     'Lisa S': 88,
-    'F.Bart': 77
+    'F.Bart': 77,
+    'a': 66
 }
 
 print_scores(**data)
 
-def print_info(name, *, gender, city='Beijing', age):
+
+def print_info(name, *args, **kw, gender, city='Beijing', age):
     print('Personal Info')
     print('---------------')
     print('   Name: %s' % name)
@@ -27,5 +31,23 @@ def print_info(name, *, gender, city='Beijing', age):
     print('    Age: %s' % age)
     print()
 
+
 print_info('Bob', gender='male', age=20)
 print_info('Lisa', gender='female', city='Shanghai', age=18)
+
+
+def print_info_all(name, master=0, *args, gender, city='Beijing', age, **kw):
+    print('Personal Info')
+    print('---------------')
+    print('    Name: %s' % name)
+    print(' Mastery: %s' % master)
+    print('  Gender: %s' % gender)
+    print('    City: %s' % city)
+    print('     Age: %s' % age)
+    print('---------------')
+    print('And other:')
+    print(args)
+    print(kw)
+
+
+print_info_all('Lisa', 72, gender='female', age=18)
