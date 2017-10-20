f5e8cd51b9d4a242d22e10a760ba81e866cd4091
ZainZhu <370262452@qq.com>
2017-10-20 17:36:21 +0800

    reformat var_args.py

--- samples/advance/do_listcompr.py
+++ samples/advance/do_listcompr.py
@@ -5,8 +5,14 @@ print([x * x for x in range(1, 11)])
 print([x * x for x in range(1, 11) if x % 2 == 0])
 print([m + n for m in 'ABC' for n in 'XYZ'])
 
-d = {'x': 'A', 'y': 'B', 'z': 'C' }
+d = {'x': 'A', 'y': 'B', 'z': 'C'}
 print([k + '=' + v for k, v in d.items()])
 
 L = ['Hello', 'World', 'IBM', 'Apple']
 print([s.lower() for s in L])
+
+L1 = ['Hello', 'World', 18, 'Apple', None]
+L2 = [s.lower() for s in L1 if isinstance(s, str)]
+print("L1 is: ", L1)
+print("L2 is: ", L2)
+
