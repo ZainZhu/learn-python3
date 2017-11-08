051a1b46c5073039b8f73a520c426146d67bd2bb
ZainZhu <370262452@qq.com>
2017-11-08 18:30:58 +0800

    reformat var_args.py

--- samples/advance/do_generator.py
+++ samples/advance/do_generator.py
@@ -6,9 +6,10 @@ print(s)
 for x in s:
     print(x)
 
-def fib(max):
+
+def fib(max_times):
     n, a, b = 0, 0, 1
-    while n < max:
+    while n < max_times:
         yield b
         a, b = b, a + b
         n = n + 1
@@ -29,3 +30,21 @@ while 1:
         print('Generator return value:', e.value)
         break
 
+
+def triangles():
+    list_triangles = [0, 1, 0]
+    while True:
+        yield list_triangles[1:-1]
+        current_p = list_triangles.__len__() - 1
+        list_triangles.append(0)
+        while current_p > 0:
+            list_triangles[current_p] += list_triangles[current_p - 1]
+            current_p -= 1
+
+
+n = 0
+for t in triangles():
+    print(t)
+    n = n + 1
+    if n == 10:
+        break
