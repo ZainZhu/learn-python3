8bc35855af83c8a030b195af4dbdcba0b4f09018
Jason Zhu <370262452@qq.com>
2018-01-02 00:49:16 +0800

    reformat var_args.py Solution of added exercises.

--- samples/functional/do_map.py
+++ samples/functional/do_map.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from functools import reduce
 
 
 def f(x):
@@ -7,3 +8,19 @@ def f(x):
 
 
 print(list(map(f, [1, 2, 3, 4, 5, 6, 7, 8, 9])))
+
+
+def normalize(name):
+    tmp = list(map(str.lower, name))
+    tmp[0] = tmp[0].upper()
+    return reduce(lambda x, y: x + y, tmp)
+
+
+# def normalize(name):
+#     tmp1 = name[0].upper()
+#     tmp2 = name[1:].lower()
+#     return tmp1 + tmp2
+
+L1 = ['adam', 'LISA', 'barT']
+L2 = list(map(normalize, L1))
+print(L2)
